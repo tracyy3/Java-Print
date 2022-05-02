fbbcb1ff26de3f8770c101bdb91928deda27051b
tyang03 <101145727+tyang03@users.noreply.github.com>
2022-05-02 12:19:47 -0400

    Delete src directory

--- src/HiFriend.java
+++
@@ -1,6 +0,0 @@
-public class HiFriend {
-    public static void main(String[] args) {
-        String hello = "Hi " + "Larry " + "Sprinkle!";
-        System.out.println(hello);
-    }
-}
--- src/Vacation.java
+++
@@ -1,9 +0,0 @@
-public class Vacation {
-    public static void main(String[] args) {
-        System.out.println("I'd like to visit");
-        System.out.println("Tokyo");
-        System.out.println("Japan");
-    }
-}
-//vacation
-
